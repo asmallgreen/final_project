ae55c81dfef9c506f5c83755d042a31055ca5f73
asmallgreen <138191593+asmallgreen@users.noreply.github.com>
2023-09-22 16:06:35 +0800

    修改初始環境

---
+++ client/src/pages/_app.js
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import '../styles/global.scss'; // 全域的 SCSS 檔案
+// 請在這裡 import自己的 scss 檔
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp;
