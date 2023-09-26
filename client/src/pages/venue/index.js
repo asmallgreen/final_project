9e974ac46bb98653b32aec0be02ceb3dfcbec6fd
KUWACHA <chester40505@gmail.com>
2023-09-26 17:24:00 +0800

    北中南場地

---
+++ client/src/pages/venue/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+export default function index() {
+  return (
+    <>
+      <Container>
+        <div>
+          <h1>北部道場</h1>
+        </div>
+        <div className=''>
+          <img src='/images/default_dojo_n.png'></img>
+        </div>
+        <div>
+          <h1>中部道場</h1>
+        </div>
+        <div className=''>
+          <img src='/images/default_dojo_n.png'></img>
+        </div>
+        <div>
+          <h1>南部道場</h1>
+        </div>
+        <div className=''>
+          <img src='/images/default_dojo_n.png'></img>
+        </div>
+      </Container>
+    </>
+  )
+}
