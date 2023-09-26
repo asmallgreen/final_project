d6d82adc0ae9ef927b81835ddfed1459f01850f3
Ciro <wei4652@gmail.com>
2023-09-26 10:12:52 +0800

    引用寬的檔案

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,12 +1,8 @@
-import { useEffect } from 'react';
-import DefaultLayout from '../components/layout/default-layout'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/_globals.scss';
+import DefaultLayout from '@/components/layout/default-layout/index.js'
+import '@/styles/index.scss';
 
 export default function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    import('bootstrap/dist/js/bootstrap')
-  }, [])
+ 
   // 使用預設排版檔案
   // 對應`components/layout/default-layout/index.js`(或components/layout/default-layout.js)
   const getLayout =
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function index() {
+export default function Index() {
   return (
-    <div>index</div>
+    <div>Here is Home Page</div>
   )
 }
