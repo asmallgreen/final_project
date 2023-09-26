a7ac159114cd595a048714c090f91b566807cdff
HuangChuan_11 <bin922898952@gmail.com>
2023-09-27 00:27:33 +0800

    coupon card完成
    coupon card完成
    手機板col排版正常
    coupon上方頁籤尚未客製

---
+++ client/src/components/coupon-card/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function CouponCard({type}) {
+  const couponLeftClass = type === 1 ? 'coupon-left-green' : 'coupon-left-red';
+  return (
+    <>
+      <div className="coupon-card">
+        <div className={` ${couponLeftClass} d-flex`}>
+          <div className="left-edge"></div>
+          <div className="text-center d-flex align-items-center">
+            <div>
+              <p>消費滿$1000</p>
+              <p>折價</p>
+              NT$<span className="display-6">900</span>
+            </div>
+          </div>
+        </div>
+        <div className="coupon-right">
+          <h5 className="fw-bold">中秋射月亮</h5>
+          <p>適用於商品分類 " 弓箭 " </p>
+          <div className="text-center time">
+            <p>優惠期限</p>
+            <p>
+              {`2023-09-01`}~{`2023-09-31`}
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- client/src/pages/member/coupon/index.js
+++ client/src/pages/member/coupon/index.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+
+import MemberSidebar from "@/components/member-sidebar";
+import Pagination from "@/components/pagination";
+import CouponCard from "@/components/coupon-card";
 
 export default function Coupon() {
   return (
     <>
-        <div>Here is member coupon</div>
+      <div className="row">
+        <div className="col-12 col-md-3 p-3">
+          <MemberSidebar />
+        </div>
+        <div className="col-12 col-md-9 my-4">
+          <h3>我的優惠券</h3>
+          <Tabs
+            defaultActiveKey="home"
+            id="justify-tab-example"
+            className="mb-3"
+            justify
+          >
+            <Tab eventKey="home" title="擁有的優惠券">
+              <div className="container">
+                <div className="row text-center">
+                  <div className="col-6">
+                    <input type="checkbox" className="form-check-input mx-2" />
+                    僅顯示可使用的優惠券
+                  </div>
+                  <div className="col-6">一鍵清空失效的優惠券</div>
+                </div>    
+                <div className="row">
+                  <div className="col-12 col-xl-6 col-xxl-4">
+                    <CouponCard type={1}/>
+                  </div>
+                  <div className="col-12 col-xl-6 col-xxl-4">
+                    <CouponCard type={1}/>
+                  </div>
+                  <div className="col-12 col-xl-6 col-xxl-4">
+                    <CouponCard type={2}/>
+                  </div>
+                </div>
+              </div>
+            </Tab>
+            <Tab eventKey="profile" title="優惠券使用紀錄">
+              優惠券使用紀錄
+            </Tab>
+          </Tabs>
+          <Pagination />
+        </div>
+      </div>
     </>
-  )
+  );
 }
