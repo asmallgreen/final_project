7a0291044d91b4a664f384def279dadde6ebaf4c
KUWACHA <chester40505@gmail.com>
2023-09-27 17:15:10 +0800

    上字

--- client/src/pages/venue/index.js
+++ client/src/pages/venue/index.js
@@ -5,23 +5,32 @@ export default function index() {
   return (
     <>
       <Container>
-        <div>
-          <h1>北部道場</h1>
+        <div className='mt-5'>
+          <div className="d-flex align-items-end">
+            <h2 className="">北部道場</h2>
+            <h5 className="dojo-name">藏月弓道場</h5>
+          </div>
+          <a href="/">
+            <img src="/images/venue/dojo_n.png"></img>
+          </a>
         </div>
-        <div className=''>
-          <img src="/images/dojo_n.png"></img>
+        <div className='mt-5'>
+          <div className="d-flex align-items-end">
+            <h2 className="">中部道場</h2>
+            <h5 className="dojo-name">岡達弓道場</h5>
+          </div>
+          <a href="/">
+            <img src="/images/venue/dojo_s.png"></img>
+          </a>
         </div>
-        <div>
-          <h1>中部道場</h1>
-        </div>
-        <div className=''>
-          <img src="/images/dojo_n.png"></img>
-        </div>
-        <div>
-          <h1>南部道場</h1>
-        </div>
-        <div className=''>
-          <img src="/images/dojo_n.png"></img>
+        <div className='mt-5 mb-5'>
+          <div className="d-flex align-items-end">
+            <h2 className="">南部道場</h2>
+            <h5 className="dojo-name">駁二弓道場</h5>
+          </div>
+          <a href="/">
+            <img src="/images/venue/dojo_m.png"></img>
+          </a>
         </div>
       </Container>
     </>
