47f6cd2b27d28b4e3d437a567c6cc57c6536acd1
Lin Yi Jun <z66500646@gmail.com>
2023-10-03 15:10:35 +0800

    1510
    切板暫存

--- client/src/components/attributes/length.js
+++ client/src/components/attributes/length.js
@@ -5,7 +5,7 @@ export default function Length() {
     <>
       <div className="atrribute">
         <div className="attr-title">軸心：</div>
-        <div className="attr-btns">
+        <div className="attr-btns-length">
           <div className="attr-btn btn">1913</div>
           <div className="attr-btn btn">2014</div>
           <div className="attr-btn btn">7620</div>
--- client/src/components/attributes/material.js
+++ client/src/components/attributes/material.js
@@ -5,7 +5,7 @@ export default function Material() {
     <>
       <div className="atrribute">
         <div className="attr-title">材質：</div>
-        <div className="attr-btns">
+        <div className="attr-btns-material">
           <div className="attr-btn btn">竹</div>
           <div className="attr-btn btn">碳</div>
           <div className="attr-btn btn">木</div>
