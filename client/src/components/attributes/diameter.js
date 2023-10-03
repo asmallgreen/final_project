c175189b7d1de16a830bd19ceeaf985c909e2e39
Lin Yi Jun <z66500646@gmail.com>
2023-10-03 12:56:10 +0800

    1255
    商品詳情RWD OK
    剩下排序、篩選

---
+++ client/src/components/attributes/diameter.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Diameter() {
+  return (
+    <>
+          <div className="atrribute">
+            <div className="attr-title">長度：</div>
+            <select class="form-select attr-select">
+              <option selected>--請選擇--</option>
+              <option value="1">85釐米</option>
+              <option value="2">95釐米</option>
+              <option value="3">105釐米</option>
+              <option value="3">115釐米</option>
+            </select>
+          </div>
+
+    </>
+  )
+}
--- client/src/pages/product/category/index.js
+++ client/src/pages/product/category/index.js
@@ -2,70 +2,76 @@ import React from "react";
 import BreadCrumb from "@/components/bread-crumb/bread-crumb";
 import { Row, Col } from "react-bootstrap";
 
-import ProductCard from "@/components/product/normal-card";
 import ProductInfoAccordion from "@/components/accordion/product-info-accordion";
 import ScrollsCircle from "@/components/scroll-btn/scrolls-circle";
-import Attributes from "@/components/product/attributes";
-import FavBtn from "@/components/product/fav-btn";
-import CartBtn from "@/components/product/cart-btn";
-import QuantityBtn from "@/components/product/quantity-btn";
+import FavBtn from "@/components/category/fav-btn";
+import CartBtn from "@/components/category/cart-btn";
+import NormalCard from "@/components/product/normal-card";
+import Description from "@/components/category/description";
+import QuantityBtn from "@/components/category/quantity-btn";
+import Material from "@/components/attributes/material";
+import Length from "@/components/attributes/length";
+import Diameter from "@/components/attributes/diameter";
 function Category() {
   return (
     <>
-      <div className="container p-0">
-        {/* 麵包屑 */}
+      {/* 麵包屑 */}
+      <div className="product-info-crum">
         <BreadCrumb />
-        {/* 產品select區 */}
-        <div className="cart row d-flex position-relative">
-          {/* 左邊區域 */}
-          <div className="d-flex position-relative img-area px-4 col-6">
-            {/* <img src="" /> */}
-            <div className="img">暫時img</div>
-            <ScrollsCircle className="scrolls" />
+      </div>
+      {/* *********************** */}
+      <Row>
+        <Col xl="4" md="5" className="product-info-img offset-md-1 offset-xl-2">
+          <div className="img">
+            <img src=""></img>
           </div>
-          {/* 右邊區域 */}
-          <div className="detail col-6 flex-column d-flex justify-content-between ">
-            {/* <div className="title">選購</div> */}
-            <div className="product-info d-flex flex-column justify-content-center align-items-center">
-              <div className="product-name my-2"> 特選黑羽雞翅 汝拉箭6枚</div>
-              <div className="price my-2">$8000</div>
-              <div className="intro my-2">採用廣受歡迎的伊斯頓硬鋁箭桿</div>
-            </div>
-            {/* select屬性 */}
-            <div className="attributes d-flex flex-column justify-content-center">
-              <Attributes />
-              <Attributes />
-              <Attributes />
-            </div>
+        </Col>
+        <Col xl="4" md="5" className="product-info-select">
+          <div className="product-info-des">
+            <Description />
+          </div>
+          {/* 屬性按鈕 */}
+          <div className="product-info-attr">
+            <Material />
+            <Length />
+            <Diameter />
+          </div>
 
-            {/* buttons */}
-            <QuantityBtn className="mx-2" />
-            <Row>
+          <div className="product-info-btns">
+            {/* 數量按鈕 */}
+            <QuantityBtn />
+            {/* 購物車 收藏按鈕 */}
+            <div className="product-info-btn">
               <FavBtn />
               <CartBtn />
-            </Row>
+            </div>
           </div>
-        </div>
-        {/* 商品資訊(手風琴) */}
-        <ProductInfoAccordion />
-      </div>
+        </Col>
+      </Row>
+
+      {/* *********************** */}
+
+      {/* 商品資訊(手風琴) */}
+      <ProductInfoAccordion />
+      {/* </div> */}
 
       {/* 相關商品推薦 */}
       <div className="product-page-title">
         <p>相關商品推薦</p>
       </div>
-      <Row className="justify-content-md-center py-5">
-        <Col md="auto" xs="2" className="cards d-flex offset-auto">
+      <Row className="normal-cards-area">
+        <Col className="normal-cards">
           <Row className="rows">
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
+            <NormalCard />
+            <NormalCard />
+            <NormalCard />
+            <NormalCard />
+            <NormalCard />
           </Row>
         </Col>
       </Row>
+
+      {/* ----------------------- */}
     </>
   );
 }
