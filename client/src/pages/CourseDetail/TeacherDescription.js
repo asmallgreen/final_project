df2c32d0cdf20fdcea87660399f055b1c567ea8e
CYL <cyl.jim@gmail.com>
2023-10-05 01:31:06 +0800

    teacherDescription 卡片完成

--- client/src/pages/CourseDetail/TeacherDescription.js
+++ client/src/pages/CourseDetail/TeacherDescription.js
@@ -1,42 +1,27 @@
 import React from "react";
 
 export default function TeacherDescription() {
+  //假資料
+  const [teacherAvatar, teacherName, teacherRank, teacherParagraph] = [
+    "https://github.com/asmallgreen/final_project/blob/Jim/client/public/images/teacher/teacher_2.jpg?raw=true",
+    "小明",
+    "鍊士 六段",
+    "小明是一個很棒的老師小明是一個很棒的老師小明是一個很棒的老師小明是一個很棒的老師小明是一個很棒的老師小明是一個很棒的老師小明是一個很棒的老師小明是一個很棒的老師小明是一個很棒的老師小明是一個很棒的老師小明是一個很棒的老師小明是一個很棒的老師小明是一個很棒的老師小明是一個很棒的老師小明是一個很棒的老師小明是一個很棒的老師小明是一個很棒的老師小明是一個很棒的老師小明是一個很棒的老師小明是一個很棒的老師小明是一個很棒的老師小明是一個很棒的老師小明是一個很棒的老師小明是一個很棒的老師"
+  ];
+
   return (
     <>
-      <div className="Teacher-description-container container row">
-        <div className="left col">
+      <div className="teacher-description-container container d-flex">
+        <div className="teacher-avatar ">
           <img
-            src="https://github.com/asmallgreen/final_project/blob/Jim/client/public/images/teacher/teacher_1.jpg?raw=true"
+            src={teacherAvatar}
             alt=""
           ></img>
         </div>
-        <div className="right col">
-          <div>
-            「初探：射法八節」課程旨在為射箭愛好者和初學者提供全面的射箭體驗。
-            <br />
-            <br />
-            射箭作為一項古老而優雅的運動，不僅考驗著技巧，還培養了專注力、精確度和自我控制等品質。
-            <br />
-            本課程的主要焦點是射法八節，這是射箭的核心技巧，包括站位、拿弓、拉弦、瞄準、放箭、射程、觀察和修正。
-            <br />
-            <br />
-            課程將首先提供射箭的基礎知識，包括射箭的歷史、器材的種類以及射箭場地的特點。
-            <br />
-            這將為學員提供一個全面的背景，幫助他們更好地理解這項運動。
-            <br />
-            隨後，我們將深入研究射法八節的每個方面。學員將學習如何正確站位，
-            <br />
-            如何穩定拿弓，如何拉弦，如何進行精確的瞄準，如何放箭，
-            <br />
-            以及如何觀察自己的射箭結果並進行必要的修正。
-            <br />
-            <br />
-            這些技巧對於培養優秀的射箭技術至關重要，並且將在實際的射箭練習中得到應用。
-            <br />
-            課程將提供大量的實踐機會，讓學員可以反復練習，逐漸提高他們的技術水平。
-            <br />
-            專業導師將提供個別指導，根據學員的需求調整課程內容，確保每個人都能夠充分發揮潛力。
-          </div>
+        <div className="teacher-intro ">
+          <h3 className="teacher-name">{teacherName}</h3>
+          <div className="teacher-rank">{teacherRank}</div>
+          <div className="teacher-paragraph">{teacherParagraph}</div>
         </div>
       </div>
     </>
