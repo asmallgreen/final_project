fa4c02833cf87fbc9bb00e490d8db948300e5ca0
HuangChuan_11 <bin922898952@gmail.com>
2023-10-04 18:34:23 +0800

    我的優惠券成功套用
    我的優惠券關聯對應member_id
    尚未建立delete過期優惠券
    尚未編輯已使用優惠券

--- client/src/components/coupon-card/index.js
+++ client/src/components/coupon-card/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 
-export default function CouponCard({ id,name, type, discount, deadline }) {
-  const couponLeftClass = type === 1 ? 'coupon-left-green' : 'coupon-left-red';
-  let couponTypeText = '';
-  let discountText = '';
+export default function CouponCard({ id, name, type, discount, deadline }) {
+  const deadlineDate = new Date(deadline);
+  const formattedDeadline = deadlineDate.toLocaleDateString();
+
+  const couponLeftClass = type === 1 ? "coupon-left-green" : "coupon-left-red";
+  let couponTypeText = "";
+  let discountText = "";
 
   if (type === 1) {
-    couponTypeText = '全館';
+    couponTypeText = "全館";
     discountText = `${discount}折`;
   } else if (type === 2) {
-    couponTypeText = '折價';
-    discountText = `$${discount}`
+    couponTypeText = "折價";
+    discountText = `$${discount}`;
   }
   return (
     <div className="coupon-card">
@@ -25,10 +28,8 @@ export default function CouponCard({ id,name, type, discount, deadline }) {
       <div className="coupon-right">
         <h5 className="fw-bold">{name}</h5>
         <div className="text-center time">
-          <p>優惠期限</p>
-          <p>
-            至 {deadline}
-          </p>
+          <p>優惠期限至</p>
+          <p>{formattedDeadline}</p>
         </div>
       </div>
     </div>
--- server/routes/memberDashboard.js
+++ server/routes/memberDashboard.js
@@ -1,27 +1,42 @@
 import express from 'express'
 const router = express.Router()
-
 import 'dotenv/config.js'
+import { executeQuery } from '../routes/database.js';
 
-import {
-    getAllCoupon
-} from "../models/member-coupon.js"
-
-
+import { getCouponList } from '../models/coupon-list.js';
 
 router.get('/', function (req, res) {
     res.send('member home page!');
 });
 
-router.get('/coupon', async (req, res) => {
-    const CouponList = await getAllCoupon()
-    return res.json({
-        message:"search success",
-        code:"200",
-        CouponList
-    })
-});
-// router.get('/order-list', function (req, res) {
-//     res.send('member order-list page!');
+// router.get('/coupon-list', async (req, res) => {
+//     const CouponList = await getCouponList()
+//     return res.json({
+//         message:"search success",
+//         code:"200",
+//         CouponList
+//     })
 // });
+router.get('/find-member-coupon', async (req, res) => {
+    const memberId = req.query.memberId
+    const sql = `SELECT coupon.id, coupon.name, coupon.discount, coupon.type, coupon.deadline
+    FROM member_coupon
+    INNER JOIN coupon ON member_coupon.coupon_id = coupon.id
+    WHERE member_coupon.member_id = ?;`
+    try {
+        const result = await executeQuery(sql, [memberId]);
+        const memberCoupon = result
+        return res.json({
+            message: "search success",
+            code: "200",
+            memberCoupon
+        });
+    } catch (error) {
+        console.error('獲取會員優惠券資料錯誤', error);
+        return res.status(500).json({
+            message: "search error",
+            code: "500"
+        });
+    }
+});
 export default router
