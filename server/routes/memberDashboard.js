d7c2ff5386ad12734556bed732e5eb507a35def5
HuangChuan_11 <bin922898952@gmail.com>
2023-10-04 22:48:17 +0800

    添加過濾路由(尚未實裝前端)
    添加過濾路由(尚未實裝前端)

--- server/routes/memberDashboard.js
+++ server/routes/memberDashboard.js
@@ -17,12 +17,16 @@ router.get('/', function (req, res) {
 //         CouponList
 //     })
 // });
+
+//讀取特定member的coupon資料
+//table: member_coupon, coupon
 router.get('/find-member-coupon', async (req, res) => {
     const memberId = req.query.memberId
     const sql = `SELECT coupon.id, coupon.name, coupon.discount, coupon.type, coupon.deadline
     FROM member_coupon
     INNER JOIN coupon ON member_coupon.coupon_id = coupon.id
-    WHERE member_coupon.member_id = ?;`
+    WHERE member_coupon.member_id = ? 
+    AND member_coupon.status = "可使用";`
     try {
         const result = await executeQuery(sql, [memberId]);
         const memberCoupon = result
@@ -39,4 +43,36 @@ router.get('/find-member-coupon', async (req, res) => {
         });
     }
 });
+
+//過濾掉過期的coupon
+router.get('/showCoupon', async (req, res) => {
+    const today = new Date(); // 取用今日日期
+    const memberId = req.query.memberId;
+    const sql = `
+        SELECT coupon.id, coupon.name, coupon.discount, coupon.type, coupon.deadline
+        FROM member_coupon
+        INNER JOIN coupon ON member_coupon.coupon_id = coupon.id
+        WHERE member_coupon.member_id = ? 
+        AND DATE(coupon.deadline) >= CURDATE();`;
+        // CURDATE()是MySQL的日期函數，表示當前日期
+
+    try {
+        const result = await executeQuery(sql, [memberId]);
+        const memberCoupon = result;
+        
+        return res.json({
+            message: "search success",
+            code: "200",
+            memberCoupon
+        });
+    } catch (error) {
+        console.error('獲取會員優惠券資料錯誤', error);
+        return res.status(500).json({
+            message: "search error",
+            code: "500"
+        });
+    }
+});
+
+//
 export default router
