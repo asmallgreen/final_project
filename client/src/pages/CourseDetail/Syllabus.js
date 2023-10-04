3afa3f88df573dc32f69aa8c668dc2031f5b49c3
cyl-jsl <cyl.jsl@gmail.com>
2023-10-04 18:41:38 +0800

    大綱的body元件化，並加入簡易樣式

--- client/src/pages/CourseDetail/Syllabus.js
+++ client/src/pages/CourseDetail/Syllabus.js
@@ -1,4 +1,5 @@
 import React from "react";
+import TableBody from "./TableBody";
 
 export default function Syllabus() {
   return (
@@ -6,60 +7,24 @@ export default function Syllabus() {
       <div className="syllabus-container container">
         <div className="row">
           <div className="col-1">
-            <div className="">週次</div>
+            <div className="table-header">週次</div>
           </div>
-          <div className="col-1">
-            <div className="">日期</div>
+          <div className="col-2">
+            <div className="table-header">日期</div>
           </div>
-          <div className="col-3">
-            <div className="">主題</div>
-          </div>
-          <div className="col-7">
-            <div className="">目標</div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-1">
-            <div className="">1</div>
-          </div>
-          <div className="col-1">
-            <div className="">2021/09/01</div>
-          </div>
-          <div className="col-3">
-            <div className="">課程介紹</div>
+          <div className="col-2">
+            <div className="table-header">主題</div>
           </div>
           <div className="col-7">
-            <div className="">認識課程內容，了解射箭的歷史和基本知識</div>
+            <div className="table-header">目標</div>
           </div>
         </div>
-        <div className="row">
-          <div className="col-1">
-            <div className="">1</div>
-          </div>
-          <div className="col-1">
-            <div className="">2021/09/01</div>
-          </div>
-          <div className="col-3">
-            <div className="">課程介紹</div>
-          </div>
-          <div className="col-7">
-            <div className="">認識課程內容，了解射箭的歷史和基本知識</div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-1">
-            <div className="">1</div>
-          </div>
-          <div className="col-1">
-            <div className="">2021/09/01</div>
-          </div>
-          <div className="col-3">
-            <div className="">課程介紹</div>
-          </div>
-          <div className="col-7">
-            <div className="">認識課程內容，了解射箭的歷史和基本知識</div>
-          </div>
-        </div>
+        <TableBody
+          week_number="第一週"
+          course_date="2021/10/10"
+          course_topic="課程介紹"
+          course_target="學員將了解課程的目標和內容。"
+        />
       </div>
     </>
   );
