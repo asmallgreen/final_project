7fea24346cacd4a1fed5fa5d1c1b847969928d48
CYL <cyl.jim@gmail.com>
2023-10-06 02:38:34 +0800

    直接抓別人的server下來改/新增course models、course routes

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-import createError from "http-errors";
-import express from "express";
-import path from "path";
+import createError from 'http-errors'
+import express from 'express'
+import path from 'path'
 // import cookieParser from 'cookie-parser'
 // import logger from 'morgan'
-import cors from "cors";
-// import session from 'express-session'
+import cors from 'cors'
+import session from 'express-session'
 // 使用檔案的session store，存在sessions資料夾
 // import sessionFileStore from 'session-file-store'
 // const FileStore = sessionFileStore(session)
@@ -16,60 +16,62 @@ const __dirname = path.dirname(__filename);
 // end 修正 __dirname
 
 // 讓console.log可以呈現檔案與行號
-import { extendLog } from "./utils/tool.js";
-extendLog(); // 執行全域套用
+// import { extendLog } from './utils/tool.js'
+// extendLog() // 執行全域套用
+// import { extendLog } from './utils/tool.js'
+// extendLog() // 執行全域套用
 // console.log呈現顏色用 全域套用
 import "colors";
 // 檔案上傳
-import fileUpload from "express-fileupload";
+// import fileUpload from 'express-fileupload'
+// import fileUpload from 'express-fileupload'
 
-// import authJwtRouter from './routes/auth-jwt.js'
+import authJwtRouter from './routes/auth-jwt.js'
+import DashboardRouter from './routes/memberDashboard.js'
 // import authRouter from './routes/auth.js'
 // import emailRouter from './routes/email.js'
 // import indexRouter from './routes/index.js'
 // import productsRouter from './routes/products.js'
 // import resetPasswordRouter from './routes/reset-password.js'
 // import usersRouter from './routes/users.js'
-// import googleLoginRouter from './routes/google-login.js'
+import googleLoginRouter from './routes/google-login.js'
 // import lineLoginRouter from './routes/line-login.js'
 // import facebookLoginRouter from './routes/facebook-login.js'
-import courseRouter from "./routes/course.js";
+
 // import favoriteRouter from './routes/favorite.js'
+import courseRouter from './routes/course.js'
 
 const app = express();
 
 // 檔案上傳
 // 選項參考: https://github.com/richardgirges/express-fileupload
-app.use(fileUpload());
+// app.use(fileUpload())
+// app.use(fileUpload())
 
 // 可以使用的CORS要求，options必要
 // app.use(cors())
 app.use(
   cors({
-    origin: [
-      "http://localhost:3005",
-      "https://localhost:9000",
-      "https://localhost:3000",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: ['http://localhost:3000', 'https://localhost:9000','https://localhost:3001','https://localhost:3005'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
 );
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
 // app.use(logger('dev'))
-app.use(express.json());
+app.use(express.json())
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }))
 // app.use(cookieParser())
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')))
 
-// // fileStore的選項
-// const fileStoreOptions = {}
-// // session-cookie使用
+// fileStore的選項
+const fileStoreOptions = {};
+// session-cookie使用
 // app.use(
 //   session({
 //     store: new FileStore(fileStoreOptions), // 使用檔案記錄session
@@ -84,31 +86,39 @@ app.use(express.static(path.join(__dirname, "public")));
 //     saveUninitialized: false,
 //   })
 // )
-
+// app.use("/product", (req, res) => {
+//   res.send("產品頁");
+// });
 // 路由使用
 // app.use('/api/', indexRouter)
-// app.get('/', (req, res)=>{
-//   res.send("首頁")
-// })
-// app.use('/api/auth-jwt', authJwtRouter)
+app.get('/', (req, res)=>{
+  res.send("首頁")
+})
+app.use('/member', authJwtRouter)
+app.use('/memberDashboard', DashboardRouter)
+// app.use('/member/coupon', memberCouponRouter)
 // app.use('/api/auth', authRouter)
 // app.use('/api/email', emailRouter)
 // app.use('/api/products', productsRouter)
 // app.use('/api/reset-password', resetPasswordRouter)
 // app.use('/api/users', usersRouter)
-// app.use('/api/google-login', googleLoginRouter)
+app.use('/google-login', googleLoginRouter)
 // app.use('/api/line-login', lineLoginRouter)
 // app.use('/api/facebook-login', facebookLoginRouter)
 // app.use('/api/favorite', favoriteRouter)
-app.use("/course", courseRouter);
-// catch 404 and forward to error handler
+app.use('/course', courseRouter)
 
 // app.listen(3005, ()=>{
-//   console.log('server started on port http://localhost:3005');
+//   console.log("服務已啟動於 http://localhost:3005");
 // })
 
+// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
+});
+
+app.listen(3005, () => {
+  console.log("服務已啟動 http://localhost:3005");
 });
 
 // error handler
@@ -118,9 +128,7 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  // 更改為錯誤訊息預設為JSON格式
-  res.status(500).send({ error: err });
-});
+  res.status(err.status || 500).json({ error: err });
+})
 
 export default app;
--- server/routes/course.js
+++ server/routes/course.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import { getAllCourse } from '../models/course.js'
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
     // res.send('course');
     // sql 公式函數
-    const allCourse = getAllProducts();
+    const allCourse = await getAllCourse();
     console.log(allCourse);
     res.json({
         message:'success to get all course',
