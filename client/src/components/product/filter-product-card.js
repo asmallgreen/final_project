641cc85bba34ad69d4719fcdce2337e5e3887aad
Lin Yi Jun <131141458+Luna153@users.noreply.github.com>
2023-10-05 17:46:16 +0800

    Luna (#11)
    * 1813
    
    bs測試檔
    
    * 1823
    
    scss測試
    
    * 1856
    
    request測試檔
    
    * 1937
    
    公版nav/footer(未加img)/botton
    
    * 0144
    
    公版ham/logo
    
    * 1314
    
    公版微調
    
    * 1741
    
    product頁切版，剩產品分類
    
    * 1156
    
    新增產品分類 / 分頁
    
    * 1757
    
    公版nav+hover
    
    * 1844
    
    電腦版公版nav hover微調
    
    * first
    
    * 0315
    
    <--切版-->
    商品詳情(上半部)
    
    * 1751
    
    商品詳情+拆元件
    
    * 2001
    
    手機板公版
    電腦版商品詳情
    
    * 1319
    
    layout merge後重新調整
    
    * 2101
    
    商品頁 手機rwd
    剩下優惠專區
    
    * 0008
    
    手機版 商品頁ok
    
    * 1255
    
    商品詳情RWD OK
    剩下排序、篩選
    
    * 1510
    
    切板暫存
    
    * 1805
    
    * 2103
    
    * 0335
    
    * 1656
    
    篩選新上架商品(當前時間與上架時間差15天)
    
    * 1736
    
    小改
    
    ---------
    
    Co-authored-by: clozjao <wei4652@gmail.com>

---
+++ client/src/components/product/filter-product-card.js
@@ -0,0 +1,41 @@
+// 產品頁=>所有商品(包含篩選過)卡片樣式
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import {
+  faCartShopping,
+  faHeart as farHeart,
+} from "@fortawesome/free-solid-svg-icons";
+
+export default function FilterProductCard(props) {
+  const { filterProduct } = props
+  console.log(filterProduct);
+  return (
+    <>
+      <div className="card">
+        <div className="img position-relative">
+          <img src={filterProduct.img1}></img>
+          <div className="tag position-absolute">NEW</div>
+          <button className="p-0 position-absolute">
+            <FontAwesomeIcon icon={faHeart} className="fa-heart" />
+            <FontAwesomeIcon icon={farHeart} className="far-heart" />
+          </button>
+        </div>
+        <div className="content">
+          
+            <div className="product-name">{filterProduct.name}</div>
+          
+          <div className="price d-flex justify-content-between my-1">
+          NT$ {filterProduct.price}
+            <button className="">
+              <FontAwesomeIcon
+                icon={faCartShopping}
+                className="fa-solid fa-cart-shopping"
+              />
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
