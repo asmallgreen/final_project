c54f216a3a9f98ff839b072d34609fd048727b6e
Lin Yi Jun <131141458+Luna153@users.noreply.github.com>
2023-10-06 13:26:44 +0800

    Luna (#16)
    * 1813
    
    bs測試檔
    
    * 1823
    
    scss測試
    
    * 1856
    
    request測試檔
    
    * 1937
    
    公版nav/footer(未加img)/botton
    
    * 0144
    
    公版ham/logo
    
    * 1314
    
    公版微調
    
    * 1741
    
    product頁切版，剩產品分類
    
    * 1156
    
    新增產品分類 / 分頁
    
    * 1757
    
    公版nav+hover
    
    * 1844
    
    電腦版公版nav hover微調
    
    * first
    
    * 0315
    
    <--切版-->
    商品詳情(上半部)
    
    * 1751
    
    商品詳情+拆元件
    
    * 2001
    
    手機板公版
    電腦版商品詳情
    
    * 1319
    
    layout merge後重新調整
    
    * 2101
    
    商品頁 手機rwd
    剩下優惠專區
    
    * 0008
    
    手機版 商品頁ok
    
    * 1255
    
    商品詳情RWD OK
    剩下排序、篩選
    
    * 1510
    
    切板暫存
    
    * 1805
    
    * 2103
    
    * 0335
    
    * 1656
    
    篩選新上架商品(當前時間與上架時間差15天)
    
    * 1736
    
    小改
    
    * 123
    
    * 2050
    
    新增卡片愛心toggle
    
    * 2103
    
    * 0240
    
    * 1015
    
    * Jim (#14)
    
    * 分支推送測試
    
    * after kuan merging, update repo structure.
    
    * 公版整合、courseList完成布局
    
    * courseDetail布局完成；tabs 測試前
    
    * 試著自己做tabs、安裝mui、antd
    
    * 完成CourseDetail的Tabs元件樣式
    
    * 完成homepage中的collection Tabs 分頁元件化(New.js、Hot.js...)
    
    homepage中的cardSlider元件待整建。其scss是jim-test/tabs.scss
    
    * TODO
    
    * 1. Homepage/CoureseSlider完成元件化
    
    2. Homepage 靜態圖片上傳
    
    * Homepage/CourseSlider完成；merge前git
    
    * homepage中所有重複區域元件化
    
    * 補上所有icon
    
    * 調整IntroCard的變數命名方式
    
    * 新增Detail頁面的Tabs元件化
    
    * 完成Tabs元件的Sticky
    
    * 新增老師靜態照片資料
    CourseDetail -> [cid]
    
    * Tabs分頁結構布局(Description、teacher、syllabus、faq)
    
    * 新增學員評價的樣式，卡片元件化
    
    * 初階FAQ假資料填寫
    
    * 大綱的body元件化，並加入簡易樣式
    
    * teacherDescription 卡片完成
    
    * server: 加入duo的package
    
    * 後端公式庫、模組化
    
    * 測試抓取課程資料CourseList/getAll
    
    * 直接抓別人的server下來改/新增course models、course routes
    
    * 解決package-lock衝突、樣式資料重複衝突
    
    * 解決app.listen衝突
    
    * Delete client/src/pages/member/coupon/index.js
    
    過期檔案
    
    * Delete client/src/pages/member/order/index.js
    
    過期無效檔案
    
    * Delete server/test
    
    無用檔案
    
    * Update index.js
    
    刪除無效指令
    
    ---------
    
    Co-authored-by: CYL <cyl.jim@gmail.com>
    Co-authored-by: HuangChuan_11 <137260244+HuangChuan11@users.noreply.github.com>
    
    * 1258(merge Jim版本)
    
    * Delete client/src/components/member-sidebar/index.js
    
    無用檔案
    
    * Update index.js
    
    F12
    
    ---------
    
    Co-authored-by: clozjao <wei4652@gmail.com>
    Co-authored-by: 寓 <134033984+cyl-jsl@users.noreply.github.com>
    Co-authored-by: CYL <cyl.jim@gmail.com>
    Co-authored-by: HuangChuan_11 <137260244+HuangChuan11@users.noreply.github.com>

---
+++ client/src/components/homepage/Season.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function Season() {
+    return(
+        <div className="cards">
+            <div className="card">
+              <div className="card-img"></div>
+              <div className="card-title">季節</div>
+              <div className="card-price">price</div>
+            </div>
+            <div className="card">
+              <div className="card-img"></div>
+              <div className="card-title">title</div>
+              <div className="card-price">price</div>
+            </div>
+            <div className="card">
+              <div className="card-img"></div>
+              <div className="card-title">title</div>
+              <div className="card-price">price</div>
+            </div>
+            <div className="card">
+              <div className="card-img"></div>
+              <div className="card-title">title</div>
+              <div className="card-price">price</div>
+            </div>
+            <div className="card">
+              <div className="card-img"></div>
+              <div className="card-title">title</div>
+              <div className="card-price">price</div>
+            </div>
+          </div>
+    )
+}
