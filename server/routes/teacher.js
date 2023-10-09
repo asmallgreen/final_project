ca18c5377f0528ddd109734898f0675d53cbda40
CYL <cyl.jim@gmail.com>
2023-10-09 20:47:14 +0800

    停用getAll系列等沒有使用到的後端路由

--- server/routes/teacher.js
+++ server/routes/teacher.js
@@ -2,17 +2,17 @@ import express from "express";
 import { getAllTeacher, getTeacherById } from "../models/teacher.js";
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  // res.send('course');
-  // sql 公式函數
-  const allTeacher = await getAllTeacher();
-  // console.log(allCourse);
-  res.json({
-    message: "success to get all teacher",
-    code: "200",
-    allTeacher,
-  });
-});
+// router.get("/", async (req, res) => {
+//   // res.send('course');
+//   // sql 公式函數
+//   const allTeacher = await getAllTeacher();
+//   // console.log(allCourse);
+//   res.json({
+//     message: "success to get all teacher",
+//     code: "200",
+//     allTeacher,
+//   });
+// });
 
 router.get("/:tid", async (req, res, next) => {
   const teacher = await getTeacherById(req.params.tid);
