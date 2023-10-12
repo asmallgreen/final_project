0edba6fda72c156e6e8f2fe932b0ac507011071e
CYL <cyl.jim@gmail.com>
2023-10-13 02:31:25 +0800

    course-detail RWD完成

--- client/src/pages/course/[cid].js
+++ client/src/pages/course/[cid].js
@@ -185,7 +185,7 @@ export default function CourseDetail() {
                   itemHoverColor: "#000",
                   itemSelectedColor: "#000",
                   horizontalItemPadding: "12px 60px",
-                  horizontalMargin: "0 0 100px 0",
+                  horizontalMargin: "0 0 50px 0",
                 },
               },
             }}
