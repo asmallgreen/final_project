dc7f46eddcb77b126739e1d505488e9b6f1e8ffa
CYL <cyl.jim@gmail.com>
2023-10-17 04:31:07 +0800

    更新 可新增rating-course資料的post API。

--- server/routes/rating-course.js
+++ server/routes/rating-course.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { getAllRatingCourse, getRatingCourseById, getRatingCourseByCourseId } from "../models/rating-couse.js";
+import { getAllRatingCourse, getRatingCourseById, getRatingCourseByCourseId, addRatingCourse } from "../models/rating-couse.js";
+import authenticate from "../middlewares/jwt.js";
 import { executeQuery } from "../models/base.js";
 const router = express.Router();
 
@@ -31,6 +32,39 @@ router.get('/member/:id', async (req, res) => {
     const { rows } = await executeQuery(sql)
 
     res.json({ rows })
+})
+
+router.post('/add', async (req, res) => {
+    console.log(req.body);
+
+    try{
+        // const member = req.member
+        // const member_id = member.id
+        const { order_id, member_id, course_id, score, comment } = req.body
+        const result = await addRatingCourse(order_id, member_id, course_id, score, comment)
+        const newComment ={
+            order_id,
+            member_id,
+            course_id,
+            score,
+            comment
+        }
+
+        if(result) {
+            return res.json({
+                message: "success to add rating-course",
+                code: "200",
+                newComment
+            })
+        }else{
+            return res.json({
+                message: "fail to add rating-course",
+                code: "400",
+            })
+        }
+    }catch{
+        console.log("error")
+    }
 })
 
 // 
