45dd5f380398b1edde909e25d898d41e58289952
clozjao <139102214+clozjao@users.noreply.github.com>
2023-10-18 15:20:46 +0800

    Ciro 1516 (#25)
    * test01
    
    * 新增listtitle stepbar
    
    * 2
    
    * step1 css切版
    
    * 引用寬的檔案
    
    * 將module.css 更新為scss
    
    * step1 電腦版css完成
    
    * 引入cart-reducer hook
    
    * 套用老師範例
    
    * index取消套用hooks
    
    * rwd製作中
    
    * rwd進行中
    
    * step1 切版完成
    
    * 新增切換步驟功能
    
    * 修改function
    
    * step2切版
    
    * Create test
    
    * 1612
    
    * 後端串接完成
    
    * 引入購物車鉤子
    
    * step1 抓到資料庫檔案 能+1 -1
    
    * step1 功能完成
    
    * step1 功能更新
    
    * step1 功能更新 含全選購買/刪除 部分選取 購買/刪除
    
    * 10/10
    
    * 1550
    
    * step2 完成
    
    * 1622
    
    * 1626
    
    * 1629
    
    * 1700
    
    * 1715
    
    * 1716
    
    * 1719
    
    * 1733
    
    * 新增加入購物車按鈕
    
    * 1012 1605
    
    * 1636
    
    * 1639
    
    * 2201
    
    * 引入會員優惠券 修改購物車(95%)
    
    * 1036
    
    * 1038
    
    * 1151
    
    * 1550
    
    * 成功獲得coupon_id 切版進行中
    
    * 細項製作中
    
    * 購物車完成
    
    * 1641
    
    * 0229
    
    * 1705
    
    * 1045
    
    * 產品細節修改
    
    * 購物車完成
    
    * 1414
    
    1414
    
    * 1426
    
    * 1507
    
    ---------
    
    Co-authored-by: HuangChuan_11 <137260244+HuangChuan11@users.noreply.github.com>

--- client/src/components/cart/stepone.js
+++ client/src/components/cart/stepone.js
@@ -1,16 +1,20 @@
-import React, { useState, useReducer, useEffect } from 'react'
+import React, { useState, useReducer, useEffect, useContext } from 'react'
 import { Container, Col } from 'react-bootstrap';
 
 import List from './list.js';
+
 import { useCart } from '@/hooks/use-cart.js';
 import { reducer } from '@/hooks/cart-reducer.js'
 import AddCartCourse from './addCartCourse.js'
 import AddCartProduct from './addCartProduct.js';
 import { useAuthJWT } from '@/hooks/use-auth-jwt';
 import axios from 'axios';
-import { set } from 'lodash';
 
-export default function StepOne({ setstepType, setDiscountPrice, setDiscountAmount }) {
+
+
+
+export default function StepOne({ setstepType, setDiscountPrice, setDiscountAmount, setCartCouponId ,setCartOriginDtl, setCartProductDtl}) {
+
 
   const [memberCoupon, setMemberCoupon] = useState([])
 
@@ -18,23 +22,35 @@ export default function StepOne({ setstepType, setDiscountPrice, setDiscountAmou
 
   const [selectedValue, setSelectedValue] = useState(1);
 
+  const [couponId, setCouponId] = useState(0)
+
+  const [discountType ,setDiscountType] = useState(0)
+
   const { authJWT, setAuthJWT } = useAuthJWT()
 
+ 
   let memberId = authJWT.memberData.id;
   useEffect(() => {
     if (memberId > 0) {
       axios.post('http://localhost:3005/cart/MemberCoupon', { memberId })
         .then((res) => {
           setMemberCoupon(res.data.memberCoupon)
-          console.log('這是res', res.data)
+          //console.log(res.data.memberCoupon)
+          // console.log('這是res', res.data)
+
         })
         .catch((err) => {
           console.log(err);
         })
+
     }
+
   }, [memberId])
 
-  const discountPrice = selectedValue > 1 && cart.productTotal > selectedValue ? parseInt(cart.productTotal - selectedValue) : parseInt(cart.productTotal * selectedValue)
+  const discountPrice =
+    selectedValue == 0 ? cart.productTotal * 1 :
+      selectedValue > 1 && cart.productTotal > selectedValue ?
+        cart.productTotal - selectedValue : selectedValue > 0 ? parseInt(cart.productTotal * selectedValue) : 0;
 
 
 
@@ -44,7 +60,33 @@ export default function StepOne({ setstepType, setDiscountPrice, setDiscountAmou
 
   const handleSelectChange = (event) => {
     const selectedOptionValue = event.target.value; // 获取选中的<option>的值
-    setSelectedValue(selectedOptionValue); // 更新状态变量的值
+
+    setCouponId(selectedOptionValue); // 更新状态变量的值
+
+
+    if (selectedOptionValue != 0) {
+      const newCouponId = memberCoupon.find((v) => Number(v.coupon_id) == selectedOptionValue)
+
+
+      // // const newCouponId = memberCoupon.find((v) => Number(v.coupon_id) == selectedOptionValue
+      // )
+
+      if (newCouponId.type == 1 ) {
+
+        setSelectedValue(newCouponId.discount / 10);
+       
+      } else if (newCouponId.type == 2 ) {
+        setSelectedValue(newCouponId.discount);
+      }
+
+      // setSelectedValue(newCouponId.discount)
+      // console.log('這是type',newCouponId.type)
+      // setDiscountType(newCouponId.type)
+    } else {
+      setSelectedValue(0)
+    }
+    
+
 
   };
   const sendDiscount = () => {
@@ -52,36 +94,52 @@ export default function StepOne({ setstepType, setDiscountPrice, setDiscountAmou
     setDiscountAmount(discountAmount)
   }
 
-
+  const sendCouponId = () => {
+    setCartCouponId(couponId)
+  }
 
   const sendData = () => {
     // 在子组件中调用父组件传递的回调函数，并传递数据
     setstepType(2);
   };
 
+  const sendProductDtl = () => {
+    // cartProducDtl == {} ? setCartFinalDtl(cartOriginDtl) : setCartFinalDtl(cartProducDtl)
+
+    // console.log("這是cartProducDtl", cartProducDtl)
+    // console.log("這是cartOriginDtl", cartOriginDtl)
+  }
+
 
 
   return (
     <Container>
       <div className="listTitle">
-        <Col xs={1} ><input
-          type='checkbox'
-          className='expand pcDNone'
-          onChange={(event) => {
-            let thisChk = event.target
 
-            document.querySelectorAll('.productList .cartChk').forEach(function (element) {
+        <Col  xs={1} >
+          
+            <input
+              type='checkbox'
+              id="selectAll"
+              className='expand pcDNone m1 '
+              onChange={(event) => {
+                let thisChk = event.target
 
-              element.checked = thisChk.checked
-              setChecked(+element.getAttribute("data-itemid"), thisChk.checked)
+                document.querySelectorAll('.productList .cartChk').forEach(function (element) {
 
-            })
-          }}
-        /></Col>
+                  element.checked = thisChk.checked
+                  setChecked(+element.getAttribute("data-itemid"), thisChk.checked)
+
+                })
+                
+              }}
+            />
+
+        </Col>
         <Col xs={10}>商品</Col>
         <Col xs={1}  ><span className='expand phoneDNone'>+</span></Col>
       </div>
-      <div className="productList d-none d-lg-flex">
+      <div className="productList d-none d-lg-flex px-4 ">
         <Col xs={1}><input
           type='checkbox'
           className='phoneDNone'
@@ -94,20 +152,22 @@ export default function StepOne({ setstepType, setDiscountPrice, setDiscountAmou
               setChecked(+element.getAttribute("data-itemid"), thisChk.checked)
 
             })
+
           }}
 
+
         /></Col>
-        <Col xs={4}><span className='phoneDNone'>商品名稱</span></Col>
-        <Col xs={2}><span className='phoneDNone'>規格</span></Col>
-        <Col xs={2}><span className='phoneDNone'>單價</span></Col>
-        <Col xs={2}><span className='phoneDNone'>數量</span></Col>
-        <Col xs={1}><span className='phoneDNone'>小計</span></Col>
+        <Col xs={4}><span className='phoneDNone pe-5'>商品名稱&nbsp;</span></Col>
+        <Col xs={1}><span className='phoneDNone pe-4'>規格&nbsp;</span></Col>
+        <Col xs={2}><span className='phoneDNone ps-2'>單價</span></Col>
+        <Col xs={2}><span className='phoneDNone ps-4'>數量</span></Col>
+        <Col xs={2}><span className='phoneDNone ps-4'>小計</span></Col>
 
 
 
       </div>
       <div>
-        <List mode={'product'} />
+        <List mode={'product'}  setCartOriginDtl={setCartOriginDtl} setCartProductDtl={setCartProductDtl}/>
       </div>
 
 
@@ -125,18 +185,21 @@ export default function StepOne({ setstepType, setDiscountPrice, setDiscountAmou
               });
             }}
           >刪除</button>
-          <AddCartProduct />
+          {/* <AddCartProduct /> */}
           <div className='couponSection'>
             <select onChange={handleSelectChange}>
-              <option value={1}>套用優惠券</option>
+              <option value={0}>套用優惠券</option>
               {memberCoupon.map((coupon) => (
-                <option value={Number(coupon.discount)}  key={coupon.id}>{coupon.name}</option>
+                <option value={Number(coupon.id)} key={coupon.id}
+
+                >{coupon.name}</option>
+
               ))}
             </select>
             <span
 
 
-            >{`共${cart.productTotalItems}項 , 商品小計${discountPrice} 元`}</span>
+            >{`共${cart.productTotalItems}項 , 商品小計 ${discountPrice} 元`}</span>
           </div>
         </Col>
       </div>
@@ -171,12 +234,12 @@ export default function StepOne({ setstepType, setDiscountPrice, setDiscountAmou
             })
           }}
         /></Col>
-        <Col xs={4}><span className='phoneDNone'>商品名稱</span></Col>
-        <Col xs={4}><span className='phoneDNone'>課程內容</span></Col>
-        <Col xs={3}><span className='phoneDNone'>小計</span></Col>
+        <Col xs={5}><span className='phoneDNone'>商品名稱</span></Col>
+        <Col xs={3}><span className='phoneDNone'>課程內容</span></Col>
+        <Col xs={3}><span className='phoneDNone ps-4 '>小計</span></Col>
       </div>
       <div>
-        <List mode="course" />
+        <List mode="course" setCartProductDtl={setCartProductDtl} setCartOriginDtl={setCartOriginDtl} />
       </div>
 
       <div className="productList">
@@ -193,9 +256,9 @@ export default function StepOne({ setstepType, setDiscountPrice, setDiscountAmou
             });
           }}
         >刪除
-        </button><div>{`共${cart.courseTotalItems}項 , 課程小計${cart.courseTotal}元`}</div>
+        </button><div>{`共${cart.courseTotalItems}項 , 課程小計 ${cart.courseTotal} 元`}</div>
         </Col>
-        <AddCartCourse />
+        {/* <AddCartCourse /> */}
       </div>
       <div className='totalSection'>
         <label>
@@ -232,6 +295,8 @@ export default function StepOne({ setstepType, setDiscountPrice, setDiscountAmou
               onClick={() => {
                 sendData();
                 sendDiscount();
+                sendCouponId();
+                sendProductDtl()
               }}
               disabled={cart.totalItems === 0}
             >下一步</button>
